a0c9a87cb087259742c8bdfb5c81cab26465bf12
joohoan <89450533+joohoan@users.noreply.github.com>
2021-08-28 02:55:09 +0900

    Update Untitled13.py

--- Untitled13.py
+++ Untitled13.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import time
 import pyupbit
 import datetime
@@ -12,19 +6,16 @@ access = "mqE7HSDocW3t6WbxrCS8ybGeaOxHOL9Aw2PXJsKX"
 secret = "lEL5u7uMedUNGSmrX1Txu8gfZ3876KrhiYSo8YkP"
 
 def get_target_price(ticker, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
-    """시작 시간 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="minute60", count=1)
     start_time = df.index[0]
     return start_time
 
 def get_balance(ticker):
-    """잔고 조회"""
     balances = upbit.get_balances()
     for b in balances:
         if b['currency'] == ticker:
@@ -35,14 +26,11 @@ def get_balance(ticker):
     return 0
 
 def get_current_price(ticker):
-    """현재가 조회"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
-# 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
-# 자동매매 시작
 while True:
     try:
         now = datetime.datetime.now()
@@ -66,7 +54,6 @@ while True:
         time.sleep(1)
 
 
-# In[ ]:
 
 
 
